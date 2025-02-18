dfc77cd43847b7400921c3f7b30c289df47dd140
Nathaly0TRajano <nathydelima00@gmail.com>
2025-02-18 11:00:16 -0300

    Alterações 18-02-2025

--- app/Livewire/Tarefa/Index.php
+++ app/Livewire/Tarefa/Index.php
@@ -2,12 +2,14 @@
 
 namespace App\Livewire\Tarefa;
 
+use App\Models\Tarefa;
 use Livewire\Component;
 
 class Index extends Component
 {
     public function render()
     {
-        return view('livewire.tarefa.index');
+        $tarefas = Tarefa::all();
+        return view('livewire.tarefa.index', compact('tarefas'));
     }
 }
--- resources/views/livewire/tarefa/index.blade.php
+++ resources/views/livewire/tarefa/index.blade.php
@@ -1,4 +1,4 @@
-<div class="mt-5">
+<div class="mt-5">                                                               
     <div class="card">
         <div class="card-body">
             <table class="table table-hover">
@@ -11,10 +11,18 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
+                    @foreach($tarefas as $t)
+                    <tr>
+                    <td>{{$t->id}}</td>
+                    <td>{{$t->nome}}</td>
+                    <td>{{$t->data_hora}}</td>
+                    <td>
+                        <button class="btn btn-info btn-sm" data-bs-toggle="modal" data-bs-target="#viewModel">Visualizar</button>
+                        <button class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#editModel">Editar</button>
+                        <button class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#deleteModel">Deletar</button>
+                    </td>
+                    </tr>
+                 @endforeach
                 </tbody>
             </table>
         </div>
